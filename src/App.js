1df9e00ae9bdf386a224d4bdd7b1e9342d920dcb
Simon Charest <simoncharest@gmail.com>
2022-02-27 23:45:56 -0500

    Adding layout

--- src/App.js
+++ src/App.js
@@ -22,4 +22,44 @@ function App() {
   );
 }
 
-export default App;
+function Game() {
+  // Grid Layout (https://www.w3schools.com/css/tryit.asp?filename=trycss_grid_layout_named)
+  return (
+    <div class="grid-container">
+      <div class="window" id="status">
+        Solo<br />
+        LV: 1<br />
+        HP: 15<br />
+        MP: 0<br />
+        G: 120<br />
+        E: 0
+      </div>
+      <div class="window" id="command-div">
+        <table id="command-table">
+          <tr>
+            <td>Fight</td><td>Spell</td>
+          </tr>
+          <tr>
+            <td>Run</td><td>Item</td>
+          </tr>
+        </table>
+      </div>
+      <div class="window" id="fighting">
+        <div id="enemy"></div>
+      </div>
+      <div class="window" id="message">
+        A Slime draws near!<br />
+        Command?<br />
+        Solo attacks!<br />
+        The Slime's Hit Points have been reduced by 2.<br />
+        The Slime attacks!<br />
+        Thy Hits decreased by 1.<br />
+        Command?<br />
+        Solo attacks!
+      </div>
+    </div>
+  );
+}
+
+//export default App;
+export default Game;
